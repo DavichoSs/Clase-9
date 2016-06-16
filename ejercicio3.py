abb365f10763bb6429536e9c7b1f7e0511f41b33
Alexander Figueroa <alexdavid2405@hotmail.es>
2016-06-16 07:40:14 -0500

    completo

---
+++ ejercicio3.py
@@ -0,0 +1,11 @@
+import time
+import turtle
+t=turtle.Pen()
+t.forward(59)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+time.sleep(60)
--- trabajoenclase9.py
+++ trabajoenclase9.py
@@ -2,3 +2,16 @@ import time
 print(time.localtime())
 '''time.struct_time(tm_year=2020, tm_mon=2, tm_mday=23, tm_hour=22, 
 	tm_min=18, tm_sec=39, tm_wday=0, tm_yday=73, tm_isdst=0)'''
+t=time.localtime()
+year=t[0]
+month=t[1]
+day=t[2]
+hour=t[3]
+minute=t[4]
+second=t[5]
+print(year)
+print(month)
+print(day)
+print(hour)
+print(minute)
+print(second)
