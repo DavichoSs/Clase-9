7bae0c82b110e9dc0973774d41c01b322e44de53
Alexander Figueroa <alexdavid2405@hotmail.es>
2016-06-16 09:38:46 -0500

    ACTUACIÓN COMPLETO

--- ejercicio8.3.py
+++ ejercicio8.3.py
@@ -1,8 +1,13 @@
 from tkinter import *
 tk=Tk()
-canvas=Canvas(tk, width=400, height=400)
+canvas=Canvas(tk, width=600, height=500)
 canvas.pack()
 
 imagen=PhotoImage(file="logo.gif")
+canvas.create_image(0,0, anchor=NW, image=imagen)
 canvas.create_image(10,10, anchor=NW, image=imagen)
+canvas.create_image(20,20, anchor=NW, image=imagen)
+canvas.create_image(30,30, anchor=NW, image=imagen)
+canvas.create_image(40,40, anchor=NW, image=imagen)
+canvas.create_image(50,50, anchor=NW, image=imagen)
 tk.mainloop()
